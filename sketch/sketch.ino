6bd00ec262d58a6468998999873b5fe14b7948b4
Magicoo51889 <jackot11f@gmail.com>
2024-02-03 17:51:36 +0000

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -8,5 +8,5 @@ void loop()
 	digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(400);                      // wait for 0.4s
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(400);                      // wait for 0.4s
+    delay(5000);                      // wait for 5s
 }
