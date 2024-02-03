83d55643848bf66371712ff32c0bb45c8ea0d2c2
Magicoo51889 <jackot11f@gmail.com>
2024-02-03 17:00:51 +0000

    Blinking Light

---
+++ sketch/sketch.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(400);                      // wait for 0.4s
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(400);                      // wait for 0.4s
+}
