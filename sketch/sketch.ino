d65329d96c210261cf20c798e3f0e3c562ce7727
Magicoo51889 <jackot11f@gmail.com>
2024-02-15 16:01:55 +0000

    Lab

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,26 +3,45 @@
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(3, OUTPUT);
-    //pinMode(INPUT_BUTTON, INPUT);
     Serial.begin(115200);
-    Serial.println("Hello, world!");
 }
 
 void loop()
 {
-    unsigned long currentTimestapMS = millis();
-    Serial.print(currentTimestapMS);
-    Serial.println("ms");
-
-    // int buttonState = digitalRead(INPUT_BUTTON);
-    // Serial.println(buttonState);
-    // delay(10);
-
-	digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    digitalWrite(3, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(400);                      // wait for 0.4s
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    digitalWrite(3, LOW);   // turn the LED off by making the voltage LOW
-    delay(5000);                      // wait for 5s
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(400);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(400);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);               
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(400);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1200);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1200);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);    
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1200);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);    
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(400);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(400);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(400);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(400);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(1200); //break before repeating the sequence
 }
