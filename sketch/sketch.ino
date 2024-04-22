55aa8c44acd69ae33fc7b14351ee8487e421e5eb
Magicoo51889 <jackot11f@gmail.com>
2024-04-22 10:41:10 +0100

    Example Analog Code

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,54 +1,15 @@
-int ledPin = 3;
-int switchPin = 7;
-
-int brightness = 0;
-int defaultBrightness = 63;
-int fadeAmount = 5;
-
-int delayTime = 100;
-
-bool prevState = LOW;
-bool stateNow = LOW;
-
-void Flash() {
-  digitalWrite(ledPin, HIGH); // turn the LED on
-  delay(delayTime); // wait for a second
-  digitalWrite(ledPin, LOW); // turn the LED off
-  delay(delayTime); // wait for a second
+void setup() {
+  Serial.begin(9600);
+  Apin = pinMode(A0, INPUT);
 }
 
-void FadeIn() {
-  for (int i = 0; i <= 255; i++) {
-    analogWrite(ledPin, i);
-    delay(delayTime);
-  }
+void loop(){
+  Serial.println("Voltage is:");
+  Serial.println(measureVoltage(A0));
 }
 
-void FadeOut() {
-  for (int i = 255; i >= 0; i--) {
-    analogWrite(ledPin, i);
-    delay(delayTime);
-  }
-}
-
-
-void setup () {
-  pinMode(ledPin, OUTPUT);
-  pinMode(switchPin, INPUT);
-}
-
-void loop () {
-  stateNow = digitalRead(switchPin);
-  if(stateNow == HIGH){
-    for (int i = 0; i <= 4; i++) { // repeat the flash 4 times
-      Flash();
-    }
-
-    for (int i = 0; i <= 3; i++) {// repeat the fade 3 times
-      FadeIn();
-      FadeOut();
-    }
-  } else {
-    analogWrite(ledPin, defaultBrightness);
-  }
+float measureVoltage(int APin){
+  int ADU = analogRead(APin);;
+  float voltage = ADU * (5.0 / 1024);
+  return voltage;
 }
